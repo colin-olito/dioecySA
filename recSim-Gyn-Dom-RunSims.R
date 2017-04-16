3ba688abf27ef4c6ee6c7e81352158dbe51f59e7
Colin Olito <colin.olito@gmail.com>
2017-04-17 09:52:02 +1000

    issue #4: again. removed threshold arg

--- recSim-Gyn-Dom-RunSims.R
+++ recSim-Gyn-Dom-RunSims.R
@@ -35,42 +35,32 @@ source('R/functions-recSim-Gyno-Dom.R')
 #  Additive effects (hf = hm = 0.5)
 
 #  Obligate Outcrossing (C = 0)
-recursionFwdSimLoop(C = 0, delta = 0, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0, delta = 0, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
 #  Partial Selfing
-recursionFwdSimLoop(C = 0.25, delta = 0.2, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0.25, delta = 0.2, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
-recursionFwdSimLoop(C = 0.25, delta = 0.8, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0.25, delta = 0.8, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
-recursionFwdSimLoop(C = 0.75, delta = 0.2, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0.75, delta = 0.2, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
-recursionFwdSimLoop(C = 0.75, delta = 0.8, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0.75, delta = 0.8, hf = 0.5, hm = 0.5, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
 ######################################
 #  Dominance Reversal (hf = hm = 0.25)
 
 
 #  Obligate Outcrossing (C = 0)
-recursionFwdSimLoop(C = 0, delta = 0, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0, delta = 0, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
 #  Partial Selfing
-recursionFwdSimLoop(C = 0.25, delta = 0.2, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0.25, delta = 0.2, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
-recursionFwdSimLoop(C = 0.25, delta = 0.8, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0.25, delta = 0.8, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
-recursionFwdSimLoop(C = 0.75, delta = 0.2, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0.75, delta = 0.2, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
-recursionFwdSimLoop(C = 0.75, delta = 0.8, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), 
-	                r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5), threshold = 1e-7)
+recursionFwdSimLoop(C = 0.75, delta = 0.8, hf = 0.25, hm = 0.25, sm.vals = c(0.05, 0.5), r.vals = c(0.0, 0.01, 0.02, 0.1, 0.2, 0.5))
 
 
 
